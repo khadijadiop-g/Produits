f2d5e2d54e459842e99101326934f9ebc768b757
khadija <khadijajjop2@gmail.com>
2026-07-01 23:41:27 +0000

    fonction d erreur et de generer Reference

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require(__DIR__ ."/controller/client.controller.php");
+require(__DIR__ ."/controller/product.controller.php");
+require(__DIR__ ."/model/product.model.php");
+require(__DIR__ ."/model/client.model.php");
+require(__DIR__ ."/model/commande.model.php");
+require(__DIR__ ."/view/client.view.php");
+require(__DIR__ ."/view/product.view.php");
+require(__DIR__ ."/service/service.php");
+require(__DIR__ ."/utils/error.utils.php");
+require(__DIR__ ."/utils/view.utils.php");
+require(__DIR__ ."/utils/validator.php");
