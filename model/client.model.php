d534b76f09825371d794184a0c215c18d99c8bef
khadija <khadijajjop2@gmail.com>
2026-07-01 21:48:59 +0000

    initialisation des tableau

---
+++ model/client.model.php
@@ -0,0 +1,6 @@
+<?php
+
+$clients = [
+    0=>['Prenom'=>'Khadija','tel'=>'770001233','address'=>'yeumbeul'],
+    1=>['Prenom'=>'Lena','tel'=>'771001233','address'=>'grand Dakar'],
+];
---
+++ model/product.model.php
@@ -0,0 +1,6 @@
+<?php
+
+$products = [
+    0=>['libele'=>'Huile','ref'=>'ref1','prix'=>2000,'quantite'=>12],
+    1=>['libele'=>'Sucre','ref'=>'ref2','prix'=>500,'quantite'=>9],
+];
