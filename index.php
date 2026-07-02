7d123c928095ac6bc520b7fb0b7872c9b8fdb8a7
khadija <khadijajjop2@gmail.com>
2026-07-02 00:47:50 +0000

    feat2 Lister les client qui n ont pas de commande

--- index.php
+++ index.php
@@ -12,9 +12,10 @@ require(__DIR__ ."/utils/view.utils.php");
 require(__DIR__ ."/utils/validator.php");
 
 
-// saveProduct();
+saveProduct();
 
-// archiverProduit();
-// ShowProduct($productsArchived);
-// ShowProduct($products);
+archiverProduit();
+ShowProduct($productsArchived);
+ShowProduct($products);
 saveClient();
+showClient();
